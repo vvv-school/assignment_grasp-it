b49ab4450026c66d39d16bb547529863d1c445ff
Ugo Pattacini <ugo.pattacini@iit.it>
2017-01-23 20:54:00 +0100

    initial commit

---
+++ smoke-test/test.cpp
@@ -0,0 +1,217 @@
+/*
+ * Copyright (C) 2016 iCub Facility - Istituto Italiano di Tecnologia
+ * Author: Ugo Pattacini <ugo.pattacini@iit.it>
+ * CopyPolicy: Released under the terms of the GNU GPL v3.0.
+*/
+
+#include <string>
+
+#include <rtf/yarp/YarpTestCase.h>
+#include <rtf/dll/Plugin.h>
+
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+#include <yarp/math/Math.h>
+#include <yarp/math/Rand.h>
+
+using namespace std;
+using namespace RTF;
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::math;
+
+/**********************************************************************/
+class TestAssignmentGraspIt : public YarpTestCase,
+                              public PortReader
+{
+    RpcClient portBall;
+    RpcClient portGI;
+    Port      portHandR;
+    Port      portHandL;
+
+    Vector ballPosRobFrame;
+    bool hit;
+
+    /******************************************************************/
+    Vector getBallPosition()
+    {
+        Bottle cmd,reply;
+        cmd.addString("world");
+        cmd.addString("get");
+        cmd.addString("ball");
+        RTF_ASSERT_ERROR_IF(portBall.write(cmd,reply),"Unable to talk to world");
+        RTF_ASSERT_ERROR_IF(reply.size()>=3,"Invalid reply from world");
+        
+        Vector pos(3);
+        pos[0]=reply.get(0).asDouble();
+        pos[1]=reply.get(1).asDouble();
+        pos[2]=reply.get(2).asDouble();
+        
+        return pos;
+    }
+
+    /******************************************************************/
+    bool setBallPosition(const Vector& pos)
+    {
+        if (pos.length()>=3)
+        {
+            Bottle cmd,reply;
+            cmd.addString("world");
+            cmd.addString("set");
+            cmd.addString("ball");
+            cmd.addDouble(pos[0]);
+            cmd.addDouble(pos[1]);
+            cmd.addDouble(pos[2]);
+            RTF_ASSERT_ERROR_IF(portBall.write(cmd,reply),"Unable to talk to world");
+            return true;
+        }
+        else
+            return false;
+    }
+
+public:
+    /******************************************************************/
+    TestAssignmentGraspIt() :
+        YarpTestCase("TestAssignmentGraspIt"),
+        hit(false)
+    {
+    }
+
+    /******************************************************************/
+    virtual ~TestAssignmentGraspIt()
+    {
+    }
+    
+    /******************************************************************/
+    virtual bool setup(yarp::os::Property& property)
+    {
+        string robot=property.check("robot",Value("icubSim")).asString();
+        float rpcTmo=(float)property.check("rpc-timeout",Value(120.0)).asDouble();
+        
+        string robotPortRName("/"+robot+"/cartesianController/right_arm/state:o");
+        string robotPortLName("/"+robot+"/cartesianController/left_arm/state:o");
+
+        string portBallName("/"+getName()+"/ball:rpc");
+        string portGIName("/"+getName()+"/gi:rpc");
+        string portHandRName("/"+getName()+"/hand/right:i");
+        string portHandLName("/"+getName()+"/hand/left:i");
+
+        portBall.open(portBallName);
+        portGI.open(portGIName);
+        portHandR.open(portHandRName);
+        portHandL.open(portHandLName);
+
+        RTF_TEST_REPORT(Asserter::format("Set rpc timeout = %g [s]",rpcTmo));
+        portBall.asPort().setTimeout(rpcTmo);
+        portGI.asPort().setTimeout(rpcTmo);
+        
+        RTF_TEST_REPORT("Connecting Ports");
+        RTF_ASSERT_ERROR_IF(Network::connect(portBallName,"/icubSim/world"),
+                            "Unable to connect to /icubSim/world");
+        RTF_ASSERT_ERROR_IF(Network::connect(portGIName,"/service"),
+                            "Unable to connect to /service");
+        RTF_ASSERT_ERROR_IF(Network::connect(robotPortRName,portHandRName),
+                            Asserter::format("Unable to connect to %s",
+                                             robotPortRName.c_str()));
+        RTF_ASSERT_ERROR_IF(Network::connect(robotPortLName,portHandLName),
+                            Asserter::format("Unable to connect to %s",
+                                             robotPortLName.c_str()));
+                
+        Rand::init();
+        
+        return true;
+    }
+
+    /******************************************************************/
+    virtual void tearDown()
+    {
+        RTF_TEST_REPORT("Closing Ports");
+        portBall.close();
+        portGI.close();
+        portHandL.close();
+        portHandR.close();
+    }
+    
+    /******************************************************************/
+    virtual bool read(ConnectionReader& reader)
+    {
+        if (!hit)
+        {
+            Bottle data;
+            data.read(reader); 
+   
+            Vector x(3);
+            x[0]=data.get(0).asDouble();
+            x[1]=data.get(1).asDouble();
+            x[2]=data.get(2).asDouble();
+
+            double d=norm(ballPosRobFrame-x);
+            if (d<0.07)
+            {
+                RTF_TEST_REPORT(Asserter::format("Great! We're at %g [m] from the ball",d));
+                hit=true;
+            }
+        }
+        
+        return true;
+    }    
+
+    /******************************************************************/
+    virtual void run()
+    {
+        Time::delay(5.0);
+
+        RTF_TEST_REPORT("Retrieving initial ball position");
+        Vector initialBallPos=getBallPosition();
+        RTF_TEST_REPORT(Asserter::format("initial ball position = (%s) [m]",
+                                         initialBallPos.toString(3,3).c_str()));
+         
+        Vector min(3,0.0),max(3,0.0);
+        min[0]= 0.0;  max[0]=0.4;   // x-axis
+        min[1]= 0.0;  max[1]=0.0;   // y-axis
+        min[2]=-0.02; max[2]=0.02;  // z-axis
+        
+        RTF_TEST_REPORT("Setting new initial ball position");
+        initialBallPos+=Rand::vector(min,max);
+        setBallPosition(initialBallPos);
+        RTF_TEST_REPORT(Asserter::format("new ball position = (%s) [m]",
+                                         initialBallPos.toString(3,3).c_str()));        
+        
+        // compute ball position in robot's root frame
+        Matrix T=zeros(4,4);
+        T(0,1)=-1.0;
+        T(1,2)=1.0;  T(1,3)=0.5976;
+        T(2,0)=-1.0; T(2,3)=-0.026;
+        T(3,3)=1.0;
+        Vector initBallPosHomog=initialBallPos;
+        initBallPosHomog.push_back(1.0);
+        ballPosRobFrame=SE3inv(T)*initBallPosHomog;
+        ballPosRobFrame.pop_back();
+
+        Bottle cmd,reply;
+        cmd.addString("look_down");
+        RTF_ASSERT_ERROR_IF(portGI.write(cmd,reply),"Unable to talk to GI");
+        RTF_ASSERT_ERROR_IF(reply.get(0).asString()=="ack","Unable to look_down");
+        cmd.clear(); reply.clear();
+        
+        RTF_TEST_REPORT("Proximity check is now active");
+        portHandR.setReader(*this);
+        portHandL.setReader(*this);
+        
+        cmd.addString("grasp_it");
+        RTF_ASSERT_ERROR_IF(portGI.write(cmd,reply),"Unable to talk to GI");
+        RTF_ASSERT_ERROR_IF(reply.get(0).asString()=="ack","Unable to grasp_it");
+        cmd.clear(); reply.clear();
+        
+        RTF_TEST_REPORT("Retrieving final ball position");
+        Vector finalBallPos=getBallPosition();
+        RTF_TEST_REPORT(Asserter::format("final ball position = (%s) [m]",
+                                         finalBallPos.toString(3,3).c_str()));
+
+        double d=norm(finalBallPos-initialBallPos);
+        RTF_TEST_CHECK(hit,"We grasped the ball!");
+        RTF_TEST_CHECK(d>0.01,Asserter::format("Ball has moved for at least %g [m]!",d));
+    }
+};
+
+PREPARE_PLUGIN(TestAssignmentGraspIt)
---
+++ src/main.cpp
@@ -0,0 +1,406 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+//
+// Author: Ugo Pattacini - <ugo.pattacini@iit.it>
+
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+#include <yarp/os/all.h>
+#include <yarp/dev/all.h>
+#include <yarp/sig/all.h>
+#include <yarp/math/Math.h>
+
+#include "helpers.h"
+
+using namespace std;
+using namespace yarp::os;
+using namespace yarp::dev;
+using namespace yarp::sig;
+using namespace yarp::math;
+
+
+/***************************************************/
+class CtrlModule: public RFModule
+{
+protected:
+    PolyDriver drvArmR, drvArmL, drvGaze;
+    PolyDriver drvHandR, drvHandL;
+    ICartesianControl *iarm;
+    IGazeControl      *igaze;
+    int startup_ctxt_arm_right;
+    int startup_ctxt_arm_left;
+    int startup_ctxt_gaze;
+
+    RpcServer rpcPort;
+    ObjectRetriever object;
+
+    /***************************************************/
+    void fixate(const Vector &x)
+    {
+        // simply look at x,
+        // but when the movement is over
+        // ensure that we'll still be looking at x
+
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    Vector computeHandOrientation(const string &hand)
+    {
+        // we have to provide a 4x1 vector representing the
+        // final orientation for the specified hand, with
+        // the palm pointing downward
+
+        // FILL IN THE CODE
+
+        // add up a further slight rotation (30 deg) around -y:
+        // this will prevent the thumb from hitting the table
+
+        // FILL IN THE CODE
+
+        return Vector(4);
+    }
+
+    /***************************************************/
+    void approachTargetWithHand(const string &hand,
+                                const Vector &x,
+                                const Vector &o)
+    {
+        // select the correct interface
+        if (hand=="right")
+            drvArmR.view(iarm);
+        else
+            drvArmL.view(iarm);
+
+        // enable all dofs but the roll of the torso
+
+        // FILL IN THE CODE
+
+        // reach the first via-point
+        // located 10 cm above the target x
+
+        // FILL IN THE CODE
+
+        // reach the final target x;
+        // well not really x, but a point 5 cm above
+
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    void liftObject(const string &hand)
+    {
+        // select the correct interface
+        if (hand=="right")
+            drvArmR.view(iarm);
+        else
+            drvArmL.view(iarm);
+
+        // just lift the hand of few centimeters
+        // wrt the current position
+
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    void moveFingers(const string &hand,
+                     const VectorOf<int> &joints,
+                     const double fingers_closure)
+    {
+        // select the correct interface
+        IControlLimits2   *ilim;
+        IPositionControl2 *ipos;
+        if (hand=="right")
+        {
+            drvHandR.view(ilim);
+            drvHandR.view(ipos);
+        }
+        else
+        {
+            drvHandL.view(ilim);
+            drvHandL.view(ipos);
+        }
+
+        // enforce [0,1] interval
+        double fingers_closure_sat=std::min(1.0,std::max(0.0,fingers_closure));
+
+        // move each finger first:
+        // if min_j and max_j are the minimum and maximum bounds of joint j,
+        // then we should move to min_j+fingers_closure_sat*(max_j-min_j)
+        for (size_t i=0; i<joints.size(); i++)
+        {
+            int j=joints[i];
+
+            // FILL IN THE CODE
+        }
+
+        // wait until all fingers have attained their set-points
+
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    void home(const string &hand)
+    {
+        Vector home_x(3);
+        home_x[0]=-0.2;
+        home_x[2]=0.08;
+
+        // select the correct interface
+        if (hand=="right")
+        {
+            drvArmR.view(iarm);
+            home_x[1]=0.3;
+        }
+        else
+        {
+            drvArmL.view(iarm);
+            home_x[1]=-0.3;
+        }
+
+        igaze->lookAtAbsAngles(Vector(3,0.0));
+        iarm->goToPositionSync(home_x);
+
+        iarm->waitMotionDone();
+        igaze->waitMotionDone();
+        igaze->setTrackingMode(false);
+    }
+
+    /***************************************************/
+    void look_down()
+    {
+        // this is a fairly easy job, isn't it?
+
+        // FILL IN THE CODE
+    }
+
+    /***************************************************/
+    bool grasp_it(const double fingers_closure)
+    {
+        Vector x; string hand;
+        if (object.getLocation(x))
+        {
+            yInfo()<<"retrieved 3D location = ("<<x.toString(3,3)<<")";
+
+            // we select the hand accordingly
+            hand=(x[1]>0.0?"right":"left");
+            yInfo()<<"selected hand = \""<<hand<<'\"';
+        }
+        else
+            return false;
+
+        fixate(x);
+        yInfo()<<"fixating at ("<<x.toString(3,3)<<")";
+
+        // refine the localization of the object
+        // with a proper hand-related map
+        if (object.getLocation(x,hand))
+        {
+            yInfo()<<"refined 3D location = ("<<x.toString(3,3)<<")";
+
+            Vector o=computeHandOrientation(hand);
+            yInfo()<<"computed orientation = ("<<o.toString(3,3)<<")";
+
+            // we set up here the lists of joints we need to actuate
+            VectorOf<int> abduction,thumb,fingers;
+            abduction.push_back(7);
+            thumb.push_back(8);
+            for (int i=9; i<16; i++)
+                fingers.push_back(i);
+
+            // let's put the hand in the pre-grasp configuration
+            moveFingers(hand,abduction,0.7);
+            moveFingers(hand,thumb,1.0);
+            moveFingers(hand,fingers,0.0);
+            yInfo()<<"prepared hand";
+
+            approachTargetWithHand(hand,x,o);
+            yInfo()<<"approached object";
+
+            moveFingers(hand,fingers,fingers_closure);
+            yInfo()<<"grasped";
+
+            liftObject(hand);
+            yInfo()<<"lifted";
+
+            moveFingers(hand,fingers,0.0);
+            yInfo()<<"released";
+
+            home(hand);
+            yInfo()<<"gone home";
+            return true;
+        }
+        return false;
+    }
+
+    /***************************************************/
+    bool openCartesian(const string &robot, const string &arm)
+    {
+        PolyDriver &drvArm=(arm=="right_arm"?drvArmR:drvArmL);
+
+        Property optArm;
+        optArm.put("device","cartesiancontrollerclient");
+        optArm.put("remote","/"+robot+"/cartesianController/"+arm);
+        optArm.put("local","/cartesian_client/"+arm);
+
+        // let's give the controller some time to warm up
+        bool ok=false;
+        double t0=Time::now();
+        while (Time::now()-t0<10.0)
+        {
+            // this might fail if controller
+            // is not connected to solver yet
+            if (drvArm.open(optArm))
+            {
+                ok=true;
+                break;
+            }
+
+            Time::delay(1.0);
+        }
+
+        if (!ok)
+        {
+            yError()<<"Unable to open the Cartesian Controller for "<<arm;
+            return false;
+        }
+        return true;
+    }
+
+public:
+    /***************************************************/
+    bool configure(ResourceFinder &rf)
+    {
+        string robot=rf.check("robot",Value("icubSim")).asString();
+
+        if (!openCartesian(robot,"right_arm"))
+            return false;
+
+        if (!openCartesian(robot,"left_arm"))
+        {
+            drvArmR.close();
+            return false;
+        }
+
+        // FILL IN THE CODE
+
+        // save startup contexts
+        drvArmR.view(iarm);
+        iarm->storeContext(&startup_ctxt_arm_right);
+
+        drvArmL.view(iarm);
+        iarm->storeContext(&startup_ctxt_arm_left);
+
+        drvGaze.view(igaze);
+        igaze->storeContext(&startup_ctxt_gaze);
+
+        rpcPort.open("/service");
+        attach(rpcPort);
+        return true;
+    }
+
+    /***************************************************/
+    bool interruptModule()
+    {
+        return true;
+    }
+
+    /***************************************************/
+    bool close()
+    {
+        drvArmR.view(iarm);
+        iarm->restoreContext(startup_ctxt_arm_right);
+
+        drvArmL.view(iarm);
+        iarm->restoreContext(startup_ctxt_arm_left);
+
+        igaze->restoreContext(startup_ctxt_gaze);
+
+        drvArmR.close();
+        drvArmL.close();
+        drvGaze.close();
+        drvHandR.close();
+        drvHandL.close();
+        rpcPort.close();
+        return true;
+    }
+
+    /***************************************************/
+    bool respond(const Bottle &command, Bottle &reply)
+    {
+        string cmd=command.get(0).asString();
+        if (cmd=="help")
+        {
+            reply.addVocab(Vocab::encode("many"));
+            reply.addString("Available commands:");
+            reply.addString("- look_down");
+            reply.addString("- grasp_it");
+            reply.addString("- quit");
+        }
+        else if (cmd=="look_down")
+        {
+            look_down();
+            // we assume the robot is not moving now
+            reply.addString("ack");
+            reply.addString("Yep! I'm looking down now!");
+        }
+        else if (cmd=="grasp_it")
+        {
+            // the "closure" accounts for how much we should
+            // close the fingers around the object:
+            // if closure == 0.0, the finger joints have to reach their minimum
+            // if closure == 1.0, the finger joints have to reach their maximum
+            double fingers_closure=0.2; // default value
+
+            // we can pass a new value via rpc
+            if (command.size()>1)
+                fingers_closure=command.get(1).asDouble();
+
+            bool ok=grasp_it(fingers_closure);
+            // we assume the robot is not moving now
+            if (ok)
+            {
+                reply.addString("ack");
+                reply.addString("Yeah! I did it! Maybe...");
+            }
+            else
+            {
+                reply.addString("nack");
+                reply.addString("I don't see any object!");
+            }
+        }
+        else
+            // the father class already handles the "quit" command
+            return RFModule::respond(command,reply);
+        return true;
+    }
+
+    /***************************************************/
+    double getPeriod()
+    {
+        return 1.0;
+    }
+
+    /***************************************************/
+    bool updateModule()
+    {
+        return true;
+    }
+};
+
+
+/***************************************************/
+int main()
+{
+    Network yarp;
+    if (!yarp.checkNetwork())
+    {
+        yError()<<"YARP doesn't seem to be available";
+        return 1;
+    }
+
+    CtrlModule mod;
+    ResourceFinder rf;
+    return mod.runModule(rf);
+}
